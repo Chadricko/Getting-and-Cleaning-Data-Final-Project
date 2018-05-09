efa5ad7a4f79906280e635122c97a18256d24d12
Chadricko <eakinc5@gmail.com>
2018-05-10 09:44:22 +1000

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -2,19 +2,19 @@ library(plyr)
 library(dplyr)
 library(tidyr)
 
-##Section 1 import variable data:
+##import variable data:
 
 featuresDF <- read.table("./UCI HAR Dataset/features.txt", header = FALSE)
 activityLabelsDF <- read.table("./UCI HAR Dataset/activity_labels.txt", header = FALSE)
 
-## Section 2 import Train Observations:
+##import Train Observations:
 
 subjectTrainDF <- read.table("./UCI HAR Dataset/train/subject_train.txt", header = FALSE)
 xTrainDF <- read.table("./UCI HAR Dataset/train/X_train.txt", header = FALSE)
 yTrainDF <- read.table("./UCI HAR Dataset/train/Y_train.txt", header = FALSE)
 
 
-##Section 3 Assign feature names to xTRainDF Columns:
+##Assign feature names to xTRainDF Columns:
 
 a <- featuresDF$V2
 counter <- 1
@@ -23,24 +23,25 @@ for (i in a){
   counter <- counter+1
 }
 
-## Section 4 Assign the appropriate feature name to 
-## yTRainDF's relevant column and assign feature name
-## to subjectTrainDF's relevant column:
+##Assign feature name to yTRainDF Column:
 
 names(yTrainDF)[1] <-paste("Activity")
+
+##Assign feature name to subjectTrainDF Column:
+
 names(subjectTrainDF)[1] <-paste("Subject")
 
-## Section 5 combine test folder dataFrames into one organized dataFrame:
+## combine test folder dataFrames into one organized dataFrame:
 
 myTrainDF <- cbind(xTrainDF, yTrainDF,subjectTrainDF)
 
-## Section 6 import Test observations:
+##import Test observations:
 
 subjectTestDF <- read.table("C:/Users/R/Desktop/JH Certificate Program/Course 3/UCI HAR Dataset/test/subject_test.txt", header = FALSE)
 xTestDF <- read.table("C:/Users/R/Desktop/JH Certificate Program/Course 3/UCI HAR Dataset/test/X_test.txt", header = FALSE)
 yTestDF <- read.table("C:/Users/R/Desktop/JH Certificate Program/Course 3/UCI HAR Dataset/test/Y_test.txt", header = FALSE)
 
-## Section 7 Assign feature names to xTestDF Columns:
+##Assign feature names to xTRainDF Columns:
 
 a <- featuresDF$V2
 counter <- 1
@@ -49,23 +50,23 @@ for (i in a){
   counter <- counter+1
 }
 
-## Section 8 Assign appropriate feature name to relevant yTestDF 
-## column and assign appropriate feature name to 
-## relevant subjectTestDF column:
-
+##Assign feature name to yTestDF Column:
 
 names(yTestDF)[1] <-paste("Activity")
+
+##Assign feature name to subjectTestDF Column:
+
 names(subjectTestDF)[1] <-paste("Subject")
 
-## Section 9 combine Test dataFrames into one organized dataFrame:
+## combine Test dataFrames into one organized dataFrame:
 
 myTestDF <- cbind(xTestDF, yTestDF,subjectTestDF)
 
-##Section 10 Combine Test and Train DataFrames:
+##Combine Test and Train DataFrames:
 
 myFinalDF <- rbind(myTrainDF,myTestDF)
 
-## Section 11 Make Activity Labels Descriptive:
+## Make Activity Labels Descriptive:
 
 activityLabelVector <- activityLabelsDF$V2
 counter <- 1
@@ -74,14 +75,14 @@ for (i in activityLabelVector){
   counter = counter+1
 }
 
-## Section 12 Extract only the measurements on the mean and
+## Extract only the measurements on the mean and
 ## standard deviation for each measurement: 
 
-a<- grep('[Ss]td',colnames(myFinalDF))
-b<- grep('\\bmean\\b',names(myFinalDF))
-myTrueDF <- myFinalDF[,c(a,b, 562,563)]
+stdcols<- grep('[Ss]td',colnames(myFinalDF))
+meancols<- grep('\\bmean\\b',names(myFinalDF))
+myTrueDF <- myFinalDF[,c(stdcols,meancols, 562,563)]
 
-## Section 13 Make column names more human level descriptive:
+## Make column names more human level descriptive:
 
 names(myTrueDF)<- paste(sub("^f", "Frequency ", names(myTrueDF)))
 names(myTrueDF)<- paste(sub("^t", "Time ", names(myTrueDF)))
@@ -96,7 +97,7 @@ names(myTrueDF)<- paste(sub("-mean()", "Means", names(myTrueDF)))
 names(myTrueDF)<- paste(sub("[(]", "", names(myTrueDF)))
 names(myTrueDF)<- paste(sub("[)]", "", names(myTrueDF)))
 
-## Section 14 Break up and reform Data into independent tidy data set 
+## Break up and reform Data into independent tidy data set 
 ## with the average of each variable for each activity and 
 ## each subject:
 
@@ -114,10 +115,9 @@ for (i in k){
       
     }
 }
-## Section 15 Change feature descriptions to reflect Data:
+## Change feature descriptions to reflect Data:
 
 names(DFQ5)<- paste(sub("Frequency ", "Mean of Frequency ", names(DFQ5)))
 names(DFQ5)<- paste(sub("Time ", "Mean of Time ", names(DFQ5)))
-
 
 
